9df5012c1445b951a340d6162d8f0e80d7829b0d
Miłosz Woźny <miloszwozny@MacBook-Air-Miosz.local>
2026-07-24 15:10:09 +0200

    Dodano kalkulator

---
+++ main.py
@@ -0,0 +1,7 @@
+print("---KALKULATOR---")
+
+liczba_a = float(input("Podaj pierwszą liczbę: "))
+liczba_b = float(input("Podaj drugą liczbę: "))
+
+wynik = liczba_a + liczba_b
+print(f"Wynik dodawania wynosi {wynik}")
